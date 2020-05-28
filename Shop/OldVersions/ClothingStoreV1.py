a394be507ba7bad25e726bf571f3f9f6a09306ae
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-05-28 14:29:02 -0700

    ClothingStoreV1.py
    Version 1 of the clothing store script file for the game.

---
+++ Shop/OldVersions/ClothingStoreV1.py
@@ -0,0 +1,37 @@
+# Start of script
+# This is the clothing store for the game
+
+# Eyeware
+
+blackSunglasses1 = bool(false) # You do not own this item
+blackSunglassesCost = int(100)
+
+# Shirts
+
+redHawaiianShirt1 = bool(false) # You do not own this item
+redHawaiianShirtCost = int(40)
+blueShirt1 = bool(false) # See above
+blueShirtCost = int(150)
+whiteShirt1 = bool(false)
+whiteShirtCost = int(100)
+
+# Hats
+
+cowBoyHat1 = bool(false) # You do not own this item
+cowBoyHatCost = int(150)
+
+"""
+Notes
+
+1. No articles of clothing are owned by default
+2. The cost (int) always refers to the main currency
+3. No other notes
+"""
+
+'''
+Welcome to the clothing store!
+
+In the clothing store, you can pick out different outfits for your dogs.
+'''
+
+# End of script
