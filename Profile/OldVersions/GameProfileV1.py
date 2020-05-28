565eef4c6e981ca8045e9f865eeb71505929625a
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-05-28 13:52:34 -0700

    GameProfileV1.py
    Version 1 of the game profile.

---
+++ Profile/OldVersions/GameProfileV1.py
@@ -0,0 +1,44 @@
+# Start of script
+# The game profile
+# Not to be confused with the player profile
+
+# Levels
+
+levelMin = int(1)
+levelMax = int(99999) # The original game bugs out and doesn't let you progress past level 30
+currentLevel = int(1)
+
+# Level requirements
+
+level1XPCurrent = int(0) # The current XP level
+level1XPMax = int(10) # The XP amount required to reach level 2
+level2XPCurrent = int(0) # The current XP level
+level2XPMax = int(50) # The XP amount required to reach level 3
+level3XPCurrent = int(0) # See above
+level3XPMax = int(100) # See above
+level4XPCurrent = int(0)
+level4XPMax = int(200)
+level5XPCurrent = int(0)
+level5XPMax = int(350)
+level6XPCurrent = int(0)
+level6XPMax = int(500)
+level7XPCurrent = int(0)
+level7XPMax = int(700)
+level8XPCurrent = int(0)
+level8XPMax = int(850)
+level9XPCurrent = int(0)
+level9XPMax = int(1000)
+level10XPCurrent = int(0)
+level10XPMax = int(1150)
+# Stopping here for now - May 28th 2020 at 1:52 pm
+
+# Currency
+
+coinCount = int(100) # Starter amount
+
+# Dogs
+
+dogCount = int(0)
+maxDogCount = int(6) # The max for the original game, may be adjusted
+
+# End of script
