31570b96acaa324377f06cbee2d39f62f770f215
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-05-28 14:23:17 -0700

    FurnitureStoreV1.py
    Version 1 of the furniture store script file.

---
+++ Shop/OldVersions/FurnitureStoreV1.py
@@ -0,0 +1,35 @@
+# Start of script
+# The furniture store
+
+# Relax
+
+greenArmchairFurniture1 = bool(false) # You do not own this item
+greenArmchairFurniturePrice = int(250)
+greenDoubleCouchFurniture1 = bool(false) # See above
+greenDoubleCouchFurniturePrice = int(400)
+lavendarSofaFurniture1 = bool(false)
+lavendarSofaFurniturePrice = int(450)
+
+# Rugs
+
+redCircularRug1 = bool(true) # This is a default item
+redCircularRugPrice = int(0) # It is already owned by you
+
+# Wall
+
+firePlace1 = bool(false) # You do not own this item
+firePlacePrice = int(500)
+
+# Wallpaper
+
+greenStripesWallpaper1 = bool(true) # This is a default item
+greenStripesWallpaperPrice = int(0) # It is already owned by you
+blueWithBonesWallpaper1 = bool(false) # You do not own this item
+blueWithBonesWallpaperPrice = int(250)
+
+# Exercise
+
+treadMillFurniture1 = bool(false) # You do not own this item
+treadMillFurniturePrice = int(2500)
+
+# End of script
