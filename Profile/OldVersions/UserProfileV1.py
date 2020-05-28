cbe91b73e45fbcb04757d91d1e87a502a2627ec9
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-05-28 14:10:40 -0700

    UserProfileV1.py
    Version 1 of the user profile script file.

---
+++ Profile/OldVersions/UserProfileV1.py
@@ -0,0 +1,66 @@
+# Start of script
+# The user profile for the game
+# Not to be confused with the game profile
+
+# Coins
+
+coinCountP1 = int(100)
+
+# Current searches
+
+couch1Search1 = bool(false)
+couch1Search1Reward = int(25)
+couch1Search2 = bool(false)
+couch1Search2Reward = int(50)
+couch1Search3 = bool(false)
+couch1Search3Reward = int(100)
+
+# Items
+
+stick1 = bool(true) # This is a default item
+foodBowlGreen1 = bool(true) # This is a default item
+purpleShampooBottle1 = bool(true) # This is a default item
+trashBag1 = bool(true) # This is a default item
+toyHelicopter1 = bool(false)
+buyToyHelicopterCost = int(400) # This item costs 400 coins
+
+# Dogs
+
+dalmation = bool(false)
+terrier = bool(false)
+# Other dogs coming soon
+
+# Your dogs
+dog1 = str("Untitled dog 1")
+dog2 = str("Untitled dog 2")
+dog3 = str("Untitled dog 3")
+dog4 = str("Untitled dog 4")
+dog5 = str("Untitled dog 5")
+dog6 = str("Untitled dog 6")
+
+# Level
+
+# See the other file (gameProfile.py)
+
+# House unlocks
+
+# UL stands for UnLocked
+livingRoomUL = bool(true)
+outbackUL = bool(true)
+upstairsUL = bool(false)
+
+# Current room
+
+# CR stands for Current Room inthis instance, NOT Carriage Return
+petStoreAisle1CR = bool(true)
+petStoreAisle2CR = bool(false)
+petStoreAisle3CR = bool(false)
+petStoreAisle4CR = bool(false)
+petStoreAisle5CR = bool(false)
+petStoreAisle6CR = bool(false)
+livingRoomCR = bool(false)
+outbackCR = bool(false)
+upstairsCR = bool(false)
+neighborhoodCR = bool(false)
+
+# End of script
